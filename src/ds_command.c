6b2d9ae8916aa2a36c4bcddf5f8c8ea233a7c314
Shane Nguyễn <shanenoi.org@gmail.com>
2026-08-08 16:14:36 +0000

    refactor: simplify command clone contracts

--- src/ds_command.c
+++ src/ds_command.c
@@ -14,7 +14,7 @@ void ds_word_vec_init(DsWordVec *vec) {
     *vec = (DsWordVec){0};
 }
 
-bool ds_word_vec_clone(DsWordVec *dst, const DsWordVec *src) {
+void ds_word_vec_clone(DsWordVec *dst, const DsWordVec *src) {
     ds_word_vec_init(dst);
     dst->len = src->len;
     dst->cap = src->len;
@@ -23,7 +23,6 @@ bool ds_word_vec_clone(DsWordVec *dst, const DsWordVec *src) {
         dst->items[i].text = ds_str_clone(src->items[i].text);
         dst->items[i].span = src->items[i].span;
     }
-    return true;
 }
 
 void ds_word_vec_free(DsWordVec *vec) {
@@ -37,11 +36,10 @@ void ds_command_stage_init(DsCommandStage *stage) {
     *stage = (DsCommandStage){0};
 }
 
-bool ds_command_stage_clone(DsCommandStage *dst, const DsCommandStage *src) {
+void ds_command_stage_clone(DsCommandStage *dst, const DsCommandStage *src) {
     ds_command_stage_init(dst);
     dst->span = src->span;
     ds_word_vec_clone(&dst->words, &src->words);
-    return true;
 }
 
 void ds_command_stage_free(DsCommandStage *stage) {
@@ -54,13 +52,12 @@ void ds_command_stage_vec_init(DsCommandStageVec *vec) {
     *vec = (DsCommandStageVec){0};
 }
 
-bool ds_command_stage_vec_clone(DsCommandStageVec *dst, const DsCommandStageVec *src) {
+void ds_command_stage_vec_clone(DsCommandStageVec *dst, const DsCommandStageVec *src) {
     ds_command_stage_vec_init(dst);
     dst->len = src->len;
     dst->cap = src->len;
     dst->items = (DsCommandStage *)ds_xcalloc(dst->cap, sizeof(DsCommandStage));
     for (size_t i = 0; i < src->len; i++) ds_command_stage_clone(&dst->items[i], &src->items[i]);
-    return true;
 }
 
 void ds_command_stage_vec_free(DsCommandStageVec *vec) {
@@ -74,13 +71,12 @@ void ds_redirect_init(DsRedirect *redirect) {
     *redirect = (DsRedirect){.kind = DS_REDIRECT_NONE};
 }
 
-bool ds_redirect_clone(DsRedirect *dst, const DsRedirect *src) {
+void ds_redirect_clone(DsRedirect *dst, const DsRedirect *src) {
     ds_redirect_init(dst);
     dst->kind = src->kind;
     dst->op_span = src->op_span;
     dst->target_span = src->target_span;
     if (src->target.len > 0) dst->target = ds_str_clone(src->target);
-    return true;
 }
 
 void ds_redirect_free(DsRedirect *redirect) {
@@ -93,11 +89,10 @@ void ds_command_init(DsCommand *command, DsCommandKind kind, DsSpan span) {
     *command = (DsCommand){.kind = kind, .span = span};
 }
 
-bool ds_command_clone(DsCommand *dst, const DsCommand *src) {
+void ds_command_clone(DsCommand *dst, const DsCommand *src) {
     ds_command_init(dst, src->kind, src->span);
     ds_command_stage_vec_clone(&dst->stages, &src->stages);
     ds_redirect_clone(&dst->redirect, &src->redirect);
-    return true;
 }
 
 void ds_command_free(DsCommand *command) {
--- src/ds_command.h
+++ src/ds_command.h
@@ -58,19 +58,19 @@ typedef struct {
 } DsCommand;
 
 void ds_word_vec_init(DsWordVec *vec);
-bool ds_word_vec_clone(DsWordVec *dst, const DsWordVec *src);
+void ds_word_vec_clone(DsWordVec *dst, const DsWordVec *src);
 void ds_word_vec_free(DsWordVec *vec);
 void ds_command_stage_init(DsCommandStage *stage);
-bool ds_command_stage_clone(DsCommandStage *dst, const DsCommandStage *src);
+void ds_command_stage_clone(DsCommandStage *dst, const DsCommandStage *src);
 void ds_command_stage_free(DsCommandStage *stage);
 void ds_command_stage_vec_init(DsCommandStageVec *vec);
-bool ds_command_stage_vec_clone(DsCommandStageVec *dst, const DsCommandStageVec *src);
+void ds_command_stage_vec_clone(DsCommandStageVec *dst, const DsCommandStageVec *src);
 void ds_command_stage_vec_free(DsCommandStageVec *vec);
 void ds_redirect_init(DsRedirect *redirect);
-bool ds_redirect_clone(DsRedirect *dst, const DsRedirect *src);
+void ds_redirect_clone(DsRedirect *dst, const DsRedirect *src);
 void ds_redirect_free(DsRedirect *redirect);
 void ds_command_init(DsCommand *command, DsCommandKind kind, DsSpan span);
-bool ds_command_clone(DsCommand *dst, const DsCommand *src);
+void ds_command_clone(DsCommand *dst, const DsCommand *src);
 void ds_command_free(DsCommand *command);
 
 #endif
